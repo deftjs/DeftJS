08e98ac513a61bffd64a693ebaf961c47065323d
John Yanarella <jmy@codecatalyst.com>
2012-03-28 23:19:22 -0400

    Updated deft.js and deft-debug.js builds.

--- build/deft-debug.js
+++ build/deft-debug.js
@@ -181,6 +181,192 @@ Ext.define('Deft.ioc.Injector', {
 });
 
 /**
+A lightweight MVC view controller.
+
+Used in conjunction with {@link Deft.mixin.Controllable}.
+*/
+Ext.define('Deft.mvc.ViewController', {
+  alternateClassName: ['Deft.ViewController'],
+  config: {
+    /**
+    		View controlled by this ViewController.
+    */
+    view: null
+  },
+  constructor: function(config) {
+    this.initConfig(config);
+    if (!this.getView() instanceof Ext.ClassManager.get('Ext.Component')) {
+      Ext.Error.raise('Error constructing ViewController: the \'view\' is not an Ext.Component.');
+    }
+    this.registeredComponents = {};
+    if (this.getView().events.initialize) {
+      this.getView().on('initialize', this.onViewInitialize, this, {
+        single: true
+      });
+    } else {
+      if (this.getView().rendered) {
+        this.init();
+      } else {
+        this.getView().on('afterrender', this.onViewInitialize, this, {
+          single: true
+        });
+      }
+    }
+    return this;
+  },
+  /**
+  	Initialize the ViewController
+  */
+  init: function() {},
+  /**
+  	Destroy the ViewController
+  */
+  destroy: function() {
+    return true;
+  },
+  /**
+  	@private
+  */
+  onViewInitialize: function() {
+    var component, config, id, listeners, _ref;
+    this.getView().on('beforedestroy', this.onViewBeforeDestroy, this);
+    this.getView().on('destroy', this.onViewDestroy, this, {
+      single: true
+    });
+    _ref = this.control;
+    for (id in _ref) {
+      config = _ref[id];
+      component = this.locateComponent(id, config);
+      listeners = Ext.isObject(config.listeners) ? config.listeners : config;
+      this.registerComponent(id, component, listeners);
+    }
+    this.init();
+  },
+  /**
+  	@private
+  */
+  onViewBeforeDestroy: function() {
+    if (this.destroy()) {
+      this.getView().un('beforedestroy', this.onBeforeDestroy, this);
+      return true;
+    }
+    return false;
+  },
+  /**
+  	@private
+  */
+  onViewDestroy: function() {
+    var id;
+    for (id in this.registeredComponents) {
+      this.unregisterComponent(id);
+    }
+  },
+  /**
+  	@private
+  */
+  getComponent: function(id) {
+    var _ref;
+    return (_ref = this.registeredComponents[id]) != null ? _ref.component : void 0;
+  },
+  /**
+  	@private
+  */
+  registerComponent: function(id, component, listeners) {
+    var event, existingComponent, getterName, listener;
+    Ext.log("Registering '" + id + "' component.");
+    existingComponent = this.getComponent(id);
+    if (existingComponent != null) {
+      Ext.Error.raise("Error registering component: an existing component already registered as '" + id + "'.");
+    }
+    this.registeredComponents[id] = {
+      component: component,
+      listeners: listeners
+    };
+    if (id !== 'view') {
+      getterName = 'get' + Ext.String.capitalize(id);
+      if (!this[getterName]) {
+        this[getterName] = Ext.Function.pass(this.getComponent, [id], this);
+      }
+    }
+    if (Ext.isObject(listeners)) {
+      for (event in listeners) {
+        listener = listeners[event];
+        Ext.log("Adding '" + event + "' listener to '" + id + "'.");
+        if (Ext.isFunction(this[listener])) {
+          component.on(event, this[listener], this);
+        } else {
+          Ext.Error.raise("Error adding '" + event + "' listener: the specified handler '" + listener + "' is not a Function or does not exist.");
+        }
+      }
+    }
+  },
+  /**
+  	@private
+  */
+  unregisterComponent: function(id) {
+    var component, event, existingComponent, getterName, listener, listeners, _ref;
+    Ext.log("Unregistering '" + id + "' component.");
+    existingComponent = this.getComponent(id);
+    if (!(existingComponent != null)) {
+      Ext.Error.raise("Error unregistering component: no component is registered as '" + id + "'.");
+    }
+    _ref = this.registeredComponents[id], component = _ref.component, listeners = _ref.listeners;
+    if (Ext.isObject(listeners)) {
+      for (event in listeners) {
+        listener = listeners[event];
+        Ext.log("Removing '" + event + "' listener from '" + id + "'.");
+        if (Ext.isFunction(this[listener])) {
+          component.un(event, this[listener], this);
+        } else {
+          Ext.Error.raise("Error removing '" + event + "' listener: the specified handler '" + listener + "' is not a Function or does not exist.");
+        }
+      }
+    }
+    if (id !== 'view') {
+      getterName = 'get' + Ext.String.capitalize(id);
+      this[getterName] = null;
+    }
+    this.registeredComponents[id] = null;
+  },
+  /**
+  	@private
+  */
+  locateComponent: function(id, config) {
+    var matches, view;
+    view = this.getView();
+    if (id === 'view') return view;
+    if (Ext.isString(config)) {
+      matches = view.query(config);
+      if (matches.length === 0) {
+        Ext.Error.raise("Error locating component: no component found matching '" + config + "'.");
+      }
+      if (matches.length > 1) {
+        Ext.Error.raise("Error locating component: multiple components found matching '" + config + "'.");
+      }
+      return matches[0];
+    } else if (Ext.isString(config.selector)) {
+      matches = view.query(config.selector);
+      if (matches.length === 0) {
+        Ext.Error.raise("Error locating component: no component found matching '" + config.selector + "'.");
+      }
+      if (matches.length > 1) {
+        Ext.Error.raise("Error locating component: multiple components found matching '" + config.selector + "'.");
+      }
+      return matches[0];
+    } else {
+      matches = view.query('#' + id);
+      if (matches.length === 0) {
+        Ext.Error.raise("Error locating component: no component found with an itemId of '" + id + "'.");
+      }
+      if (matches.length > 1) {
+        Ext.Error.raise("Error locating component: multiple components found with an itemId of '" + id + "'.");
+      }
+      return matches[0];
+    }
+  }
+});
+
+/**
 A mixin that marks a class as participating in dependency injection.
 
 Used in conjunction with {@link Deft.ioc.Injector}.
@@ -193,6 +379,33 @@ Ext.define('Deft.mixin.Injectable', {
   onClassMixedIn: function(targetClass) {
     targetClass.prototype.constructor = Ext.Function.createInterceptor(targetClass.prototype.constructor, function() {
       return Deft.Injector.inject(this.inject, this);
+    });
+  }
+});
+
+/**
+A mixin that creates and attaches the specified view controller(s) to the target view.
+
+Used in conjunction with {@link Deft.mvc.ViewController}.
+*/
+Ext.define('Deft.mixin.Controllable', {
+  requires: ['Deft.mvc.ViewController'],
+  /**
+  	@private
+  */
+  onClassMixedIn: function(targetClass) {
+    targetClass.prototype.constructor = Ext.Function.createSequence(targetClass.prototype.constructor, function() {
+      var controllerClass, controllers, _i, _len;
+      if (!(this.controller != null)) {
+        Ext.Error.raise('Error initializing Controllable instance: \`controller\` is null.');
+      }
+      controllers = Ext.isArray(this.controller) ? this.controller : [this.controller];
+      for (_i = 0, _len = controllers.length; _i < _len; _i++) {
+        controllerClass = controllers[_i];
+        Ext.create(controllerClass, {
+          view: this
+        });
+      }
     });
   }
 });
