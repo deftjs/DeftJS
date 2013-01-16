8a5b0f23ea98860950161ecfada40d70b1e333e5
Brian Kotek <brian428@gmail.com>
2013-01-16 10:23:57 -0500

    Bump version to 0.8.0.

--- build/deft.js
+++ build/deft.js
@@ -1,5 +1,5 @@
 /*!
-DeftJS 0.8.0pre
+DeftJS 0.8.0
 
 Copyright (c) 2012 [DeftJS Framework Contributors](http://deftjs.org)
 Open source under the [MIT License](http://en.wikipedia.org/wiki/MIT_License).
