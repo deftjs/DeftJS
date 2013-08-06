018e813d3d55e9cf864ba90dc84066392a86f773
John Yanarella <jmy@codecatalyst.com>
2013-08-06 19:30:47 -0400

    Bumped version to v0.9.1 for deployment in Deft JS Sencha Cmd repo.

--- packages/deft/build/deft.js
+++ packages/deft/build/deft.js
@@ -1,5 +1,5 @@
 /*!
-DeftJS 0.9.0-pre
+DeftJS 0.9.1
 
 Copyright (c) 2012-2013 [DeftJS Framework Contributors](http://deftjs.org)
 Open source under the [MIT License](http://en.wikipedia.org/wiki/MIT_License).
